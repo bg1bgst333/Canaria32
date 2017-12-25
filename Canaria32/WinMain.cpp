e26b0af316d0ba80d113151ab975cd7ab5523e87
bg1bgst333 <bg1bgst333@gmail.com>
2017-12-25 16:05:33 +0900

    -ピクチャーコントロールにスクロールバー設置.

--- Canaria32/WinMain.cpp
+++ Canaria32/WinMain.cpp
@@ -70,6 +70,8 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam){
 	// スタティック変数の初期化.
 	static HWND hPicture = NULL;	// ピクチャーコントロールハンドルhPictureをNULLで初期化.
 	static HBITMAP hBitmap = NULL;	// ビットマップハンドルhBitmapをNULLで初期化.
+	static SCROLLINFO scrollInfo = {0};	// スクロール情報構造体scrollInfoを{0}で初期化.
+	static WNDPROC lpfnWndProc = NULL;	// 既定のウィンドウプロシージャlpfnWndProcをNULLで初期化.
 
 	// ウィンドウメッセージの処理.
 	switch (uMsg){	// uMsgの値ごとに処理を振り分ける.
@@ -88,7 +90,27 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam){
 
 				// ピクチャーコントロールの作成
 				//hPicture = CreateWindow(_T("Static"), _T(""), WS_CHILD | WS_VISIBLE | WS_BORDER | SS_BITMAP | SS_REALSIZEIMAGE, 0, 0, 640, 480, hwnd, (HMENU)(WM_APP + 1), lpCS->hInstance, NULL);	// CreateWindowでピクチャーコントロールhPictureを作成.(ウィンドウクラス名は"Static".SS_REALSIZEIMAGEなので画像サイズに合わせてコントロールのサイズも変化.)
-				hPicture = CreateWindow(_T("Static"), _T(""), WS_CHILD | WS_VISIBLE | WS_BORDER | SS_BITMAP | SS_REALSIZECONTROL, 0, 0, 640, 480, hwnd, (HMENU)(WM_APP + 1), lpCS->hInstance, NULL);	// CreateWindowでピクチャーコントロールhPictureを作成.(ウィンドウクラス名は"Static".SS_REALSIZECONTROLなのでコントロールサイズに合わせて画像が拡大縮小される.)
+				hPicture = CreateWindow(_T("Static"), _T(""), WS_CHILD | WS_VISIBLE | WS_BORDER | WS_HSCROLL | WS_VSCROLL | SS_BITMAP | SS_REALSIZECONTROL, 0, 0, 640, 480, hwnd, (HMENU)(WM_APP + 1), lpCS->hInstance, NULL);	// CreateWindowでピクチャーコントロールhPictureを作成.(ウィンドウクラス名は"Static".SS_REALSIZECONTROLなのでコントロールサイズに合わせて画像が拡大縮小される.)
+
+				// ピクチャーコントロール既定のウィンドウプロシージャを取得し, WindowProcに差し替える.
+				lpfnWndProc = (WNDPROC)GetWindowLong(hPicture, GWL_WNDPROC);	// GetWindowLongでプロシージャlpfnWndProcを取得.
+				SetWindowLong(hPicture, GWL_WNDPROC, (LONG)WindowProc);	// SetWindowLongでWindowProcをセット.
+
+				// スクロールバー設定.
+				// 水平方向の設定.
+				scrollInfo.cbSize = sizeof(SCROLLINFO);	// サイズはsizeof(SCROLLINFO).
+				scrollInfo.fMask = SIF_PAGE | SIF_RANGE;	// 設定する項目のフラグはページとレンジ.
+				scrollInfo.nPage = 50;
+				scrollInfo.nMin = 0;	// 最小値は0.
+				scrollInfo.nMax = 400;
+				SetScrollInfo(hPicture, SB_HORZ, &scrollInfo, FALSE);	// SetScrollInfoでscrollInfoをセット.
+				// 垂直方向の設定.
+				scrollInfo.cbSize = sizeof(SCROLLINFO);	// サイズはsizeof(SCROLLINFO).
+				scrollInfo.fMask = SIF_PAGE | SIF_RANGE;	// 設定する項目のフラグはページとレンジ.
+				scrollInfo.nPage = 50;
+				scrollInfo.nMin = 0;	// 最小値は0.
+				scrollInfo.nMax = 400;
+				SetScrollInfo(hPicture, SB_VERT, &scrollInfo, FALSE);	// SetScrollInfoでscrollInfoをセット.
 
 				// 常にウィンドウ作成に成功するものとする.
 				return 0;	// 0を返すと, ウィンドウ作成に成功したということになる.
@@ -189,6 +211,316 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam){
 
 						// 既定の処理へ向かう.
 						break;	// breakで抜けて, 既定の処理(DefWindowProc)へ向かう.
+
+				}
+
+			}
+
+			// 既定の処理へ向かう.
+			break;	// breakで抜けて, 既定の処理(DefWindowProc)へ向かう.
+
+		// 水平方向のスクロールが発生した時.
+		case WM_HSCROLL:
+
+			// WM_HSCROLLブロック
+			{
+				
+				// hwndがhPictureの時は, 元々のウィンドウプロシージャに任せる.
+				if (hwnd == hPicture){	// hwndとhPictureが同じ時.
+
+					// スクロール情報取得.
+					UINT nSBCode = LOWORD(wParam);	// LOWORD(wParam)でスクロール動作の種類を取得.
+					UINT nPos = HIWORD(wParam);	// HIWORD(wParam)でスクロールバーの位置を取得.
+					scrollInfo.fMask = SIF_POS;	// 位置のみ変更.
+					GetScrollInfo(hwnd, SB_HORZ, &scrollInfo);	// GetScrollInfoで位置情報の取得.
+
+					// スクロールバー処理.
+					switch (nSBCode){	// nSBCodeの値で処理を振り分ける.
+
+						// 一番左
+						case SB_LEFT:
+
+							// SB_LEFTブロック
+							{
+
+								// 位置を最小値に.
+								scrollInfo.nPos = scrollInfo.nMin;	// nMinをセット.
+
+							}
+
+							// 抜ける.
+							break;	// breakで抜ける.
+
+						// 一番右
+						case SB_RIGHT:
+
+							// SB_RIGHTブロック
+							{
+
+								// 位置を最大値に.
+								scrollInfo.nPos = scrollInfo.nMax;	// nMaxをセット.
+
+							}
+
+							// 抜ける.
+							break;	// breakで抜ける.
+
+						// 1列左
+						case SB_LINELEFT:
+
+							// SB_LINELEFTブロック
+							{
+
+								// nPosが0でなければデクリメント.
+								if (scrollInfo.nPos > 0){
+									scrollInfo.nPos--;	// nPosをデクリメント.
+								}
+
+							}
+
+							// 抜ける.
+							break;	// breakで抜ける.
+
+						// 1列右
+						case SB_LINERIGHT:
+
+							// SB_LINERIGHTブロック
+							{
+
+								// nPosが最大値-1でなければインクリメント.
+								if (scrollInfo.nPos < scrollInfo.nMax - 1){
+									scrollInfo.nPos++;	// nPosをインクリメント.
+								}
+
+							}
+
+							// 抜ける.
+							break;	// breakで抜ける.
+
+						// 1ページ左
+						case SB_PAGELEFT:
+
+							// SB_PAGELEFTブロック
+							{
+
+								// nPage分減らす.
+								scrollInfo.nPos -= scrollInfo.nPage;	// nPosからnPage分引く.
+
+							}
+
+							// 抜ける.
+							break;	// breakで抜ける.
+
+						// 1ページ右
+						case SB_PAGERIGHT:
+
+							// SB_PAGERIGHTブロック
+							{
+
+								// nPage分増やす.
+								scrollInfo.nPos += scrollInfo.nPage;	// nPosからnPage分足す.
+
+							}
+
+							// 抜ける.
+							break;	// breakで抜ける.
+
+						// つまみをドラッグ中.
+						case SB_THUMBTRACK:
+
+							// SB_THUMBTRACKブロック
+							{
+
+								// 引数のnPosをセット.
+								scrollInfo.nPos = nPos;	// 引数のnPosをscrollInfo.nPosにセット.
+
+							}
+
+							// 抜ける.
+							break;	// breakで抜ける.
+
+						// つまみをドラッグ後.
+						case SB_THUMBPOSITION:
+
+							// SB_THUMBPOSITIONブロック
+							{
+
+								// 引数のnPosをセット.
+								scrollInfo.nPos = nPos;	// 引数のnPosをscrollInfo.nPosにセット.
+
+							}
+
+							// 抜ける.
+							break;	// breakで抜ける.
+
+						// それ以外.
+						default:
+
+							// 抜ける.
+							break;	// breakで抜ける.
+
+					};
+
+					// スクロール情報設定.
+					SetScrollInfo(hwnd, SB_HORZ, &scrollInfo, TRUE);	// SetScrollInfoでスクロール情報をセット.
+
+					// 元々のピクチャーコントロールのウィンドウプロシージャに任せる.
+					return CallWindowProc(lpfnWndProc, hwnd, uMsg, wParam, lParam);	// CallWindowProcでlpfnWndProcに任せる.
+
+				}
+
+			}
+
+			// 既定の処理へ向かう.
+			break;	// breakで抜けて, 既定の処理(DefWindowProc)へ向かう.
+
+		// 垂直方向のスクロールが発生した時.
+		case WM_VSCROLL:
+
+			// WM_VSCROLLブロック
+			{
+				
+				// hwndがhPictureの時は, 元々のウィンドウプロシージャに任せる.
+				if (hwnd == hPicture){	// hwndとhPictureが同じ時.
+
+					// スクロール情報取得.
+					UINT nSBCode = LOWORD(wParam);	// LOWORD(wParam)でスクロール動作の種類を取得.
+					UINT nPos = HIWORD(wParam);	// HIWORD(wParam)でスクロールバーの位置を取得.
+					scrollInfo.fMask = SIF_POS;	// 位置のみ変更.
+					GetScrollInfo(hwnd, SB_VERT, &scrollInfo);	// GetScrollInfoで位置情報の取得.
+
+					// スクロールバー処理.
+					switch (nSBCode){	// nSBCodeの値で処理を振り分ける.
+
+						// 一番上
+						case SB_TOP:
+
+							// SB_TOPブロック
+							{
+
+								// 位置を最小値に.
+								scrollInfo.nPos = scrollInfo.nMin;	// nMinをセット.
+
+							}
+
+							// 抜ける.
+							break;	// breakで抜ける.
+
+						// 一番下
+						case SB_BOTTOM:
+
+							// SB_BOTTOMブロック
+							{
+
+								// 位置を最大値に.
+								scrollInfo.nPos = scrollInfo.nMax;	// nMaxをセット.
+
+							}
+
+							// 抜ける.
+							break;	// breakで抜ける.
+
+						// 1列上
+						case SB_LINEUP:
+
+							// SB_LINEUPブロック
+							{
+
+								// nPosが0でなければデクリメント.
+								if (scrollInfo.nPos > 0){
+									scrollInfo.nPos--;	// nPosをデクリメント.
+								}
+
+							}
+
+							// 抜ける.
+							break;	// breakで抜ける.
+
+						// 1列下
+						case SB_LINEDOWN:
+
+							// SB_LINEDOWNブロック
+							{
+
+								// nPosが最大値-1でなければインクリメント.
+								if (scrollInfo.nPos < scrollInfo.nMax - 1){
+									scrollInfo.nPos++;	// nPosをインクリメント.
+								}
+
+							}
+
+							// 抜ける.
+							break;	// breakで抜ける.
+
+						// 1ページ上
+						case SB_PAGEUP:
+
+							// SB_PAGEUPブロック
+							{
+
+								// nPage分減らす.
+								scrollInfo.nPos -= scrollInfo.nPage;	// nPosからnPage分引く.
+
+							}
+
+							// 抜ける.
+							break;	// breakで抜ける.
+
+						// 1ページ下
+						case SB_PAGEDOWN:
+
+							// SB_PAGEDOWNブロック
+							{
+
+								// nPage分増やす.
+								scrollInfo.nPos += scrollInfo.nPage;	// nPosからnPage分足す.
+
+							}
+
+							// 抜ける.
+							break;	// breakで抜ける.
+
+						// つまみをドラッグ中.
+						case SB_THUMBTRACK:
+
+							// SB_THUMBTRACKブロック
+							{
+
+								// 引数のnPosをセット.
+								scrollInfo.nPos = nPos;	// 引数のnPosをscrollInfo.nPosにセット.
+
+							}
+
+							// 抜ける.
+							break;	// breakで抜ける.
+
+						// つまみをドラッグ後.
+						case SB_THUMBPOSITION:
+
+							// SB_THUMBPOSITIONブロック
+							{
+
+								// 引数のnPosをセット.
+								scrollInfo.nPos = nPos;	// 引数のnPosをscrollInfo.nPosにセット.
+
+							}
+
+							// 抜ける.
+							break;	// breakで抜ける.
+
+						// それ以外.
+						default:
+
+							// 抜ける.
+							break;	// breakで抜ける.
+
+					};
+
+					// スクロール情報設定.
+					SetScrollInfo(hwnd, SB_VERT, &scrollInfo, TRUE);	// SetScrollInfoでスクロール情報をセット.
+
+					// 元々のピクチャーコントロールのウィンドウプロシージャに任せる.
+					return CallWindowProc(lpfnWndProc, hwnd, uMsg, wParam, lParam);	// CallWindowProcでlpfnWndProcに任せる.
 
 				}
 
