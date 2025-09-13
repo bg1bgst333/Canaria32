917be138f0e425bda85b861fab9c8d5ee2bd75a6
bg1bgst333 <bg1bgst333@gmail.com>
2025-09-14 04:16:43 +0900

    vs8用プロジェクト追加.

---
+++ src/vs8/Canaria32/Canaria32/WinMain.cpp
@@ -0,0 +1,15 @@
+// ヘッダのインクルード
+// 既定のヘッダ
+#include <windows.h>	// 標準WindowsAPI
+#include <tchar.h>		// TCHAR型
+
+// _tWinMain関数の定義
+int WINAPI _tWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPTSTR lpCmdLine, int nShowCmd){
+
+	// メッセージボックスで"Canaria32"と表示.
+	MessageBox(NULL, _T("Canaria32"), _T("Canaria32"), MB_OK);	// MessageBoxで"Canaria32"と表示.
+
+	// プログラムの正常終了
+	return 0;	// 0を返して正常終了.
+
+}
